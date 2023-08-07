0fd774246bbfac55597950119aaca863d3b0fc02
Aldhi <aldhiwiduri@gmail.com>
2023-08-07 20:55:09 +0700

    golang udemy

---
+++ belajar-golang-dasar/variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Aldhi Rizkiyansah"
+	fmt.Println(name)
+
+	name = "Aldhi Rizki"
+	fmt.Println(name)
+
+	var friendName = "abidin"
+	fmt.Println(friendName)
+
+	var age = 26
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Printf("Country = %s\n", country)
+
+	var(
+		firstName = "Aldhi"
+		lastName = "Rizkiyansah"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
